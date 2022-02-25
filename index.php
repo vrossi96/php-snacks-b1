fd45c1372cf0926b79e4412edd2dbed1c05748a3
vrossi96 <vrossi396@yahoo.it>
2022-02-25 17:44:05 +0100

    start snack 2

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-/* Snack 1
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+/* 
 Snack 2
-Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
-Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
 Snack 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
 
@@ -15,6 +13,8 @@ Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro arra
 */
 
 //# SNACK 1
+// |Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
 $user_name = $_GET['name'];
 $user_mail = $_GET['mail'];
 $user_age = $_GET['age'];
@@ -22,6 +22,49 @@ $user_input_s1 = 'Accesso negato';
 if (strlen($user_name) > 3 && strpos($user_mail, '@') && strpos($user_mail, '.')) {
    $user_input_s1 = 'Accesso riuscito';
 };
+//# SNACK 2
+// Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+//# Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
+$posts = [
+   '10/01/2019' => [
+      [
+         'title' => 'Post 1',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 1'
+      ],
+      [
+         'title' => 'Post 2',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 2'
+      ],
+   ],
+   '10/02/2019' => [
+      [
+         'title' => 'Post 3',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 3'
+      ]
+   ],
+   '15/05/2019' => [
+      [
+         'title' => 'Post 4',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 4'
+      ],
+      [
+         'title' => 'Post 5',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 5'
+      ],
+      [
+         'title' => 'Post 6',
+         'author' => 'Michele Papagni',
+         'text' => 'Testo post 6'
+      ]
+   ],
+];
+var_dump(array_keys($posts));
 ?>
 
 <!DOCTYPE html>
@@ -44,6 +87,15 @@ if (strlen($user_name) > 3 && strpos($user_mail, '@') && strpos($user_mail, '.')
       <button type="submit">Invia</button>
    </form>
    <h1><?php echo $user_input_s1 ?></h1>
+   <!-- SNACK 2 -->
+   <h1>SNACK 2</h1>
+   <ul>
+      <?php for ($i = 0; $i < count($posts); $i++) : ?>
+         <li><?php DATA:
+               $posts[$i] ?>
+         </li>
+      <?php endfor; ?>
+   </ul>
 </body>
 
 </html>
