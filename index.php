bc424d5c7a9c8ccb64598862f7418513f5bfacc2
vrossi96 <vrossi396@yahoo.it>
2022-02-27 15:32:37 +0100

    fix

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ $div_big_par = explode('.', $big_par);
 
 
 //# SNACK 5
-// Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+// |Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
 
 $db = [
    'teachers' => [
